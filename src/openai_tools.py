fe7465b5ed8cfdaa61ede7b339a0e749821cef19
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2024-06-27 15:13:21 +0300

    changes to prompt

--- src/openai_tools.py
+++ src/openai_tools.py
@@ -1,19 +1,19 @@
 functions = [
-    {"type": "function", "name": "get_conversation_summary",
+    {"type": "function", "name": "end_conversation",
      "function":
     {
-        "description": "create conversation summary based on the conversation",
+        "description": "trigger an end of the conversation",
         "parameters": {
             "type": "object",
             "properties": {
+                # {}
                 "conversation_summary": {
                     "type": "string",
                     "description": "a summary of the conversation",
                 }
-
             },
             "required": ["conversation_summary"]
-        },
+        }
     }
     }
 ]
@@ -53,9 +53,14 @@ KOLZCHUT = """
 SYSTEM_PROMPT = \
     f"""
     Your name is ISRAELA. Your serve as part of a team that gathers information to help Israeli citizens understand their rights and how to exercise them.
-    Your role is gathering initial relevant information about the user and create a summary of the relevant details.
-    Your task is to personalize the questions you ask based on the specific user characteristics. Start the conversation by introducing yourself  - הי, אני ישראלה, ואני יכולה לעזור לך לקבל את מה שמגיע לך מהמדינה. כיצד אני יכולה לעזור לך? - שמג manage the dialog as if you are a social worker who is highly empathic to the user's situation and wants to help them get answers. 
+    Your role is asking the user initial questions about his status until you can create a profile for him. Avoid repeating questions you already have answers to.
+    Your task is to personalize the questions you ask based on the specific user characteristics. manage the dialog as if you are a social worker who is highly empathic to the user's situation and wants to help them get answers. 
     Your answers must be short and concise yet empathetic.
+    After you ask a few questions and have gathered enough information about the user, ask the user if he would like to provide more information.
+    If the user wants to continue, continue for a few more questions. 
+    Otherwise, return a final message starting with the prefix ### פרופיל משתמש ### with the user's profile, the profile should contain a short sentence summarizing the user's status.
+    
+    TABLE 1 contains topics relevant to rights in israel to ask the user relevant questions.
     ### TABLE 1 ###
     {KOLZCHUT}
     """
