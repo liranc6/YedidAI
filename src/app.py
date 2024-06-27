f1db7dcb9b2c9df65c25584b249edfcc51e9083f
Shahar Spencer <shahar.spencer@mail.huji.ac.il>
2024-06-27 17:12:28 +0300

    changes to prompt

--- src/app.py
+++ src/app.py
@@ -16,7 +16,6 @@ search_engine = SemanticSearchEngine()
 llm = AnthropicWrapper()
 def process_query(query):
 
-    
     data_file = '../data/dataset.json'
     embeddings_file = '../data/embeded_data.json'
     
@@ -27,13 +26,13 @@ def process_query(query):
     else:
         search_engine.load_embeddings(embeddings_file)
     
-    results= search_engine.run_search(query, top_k=5)
-    print("\nSemantic Search Results:")
-    for result in results:
-        print(f"Score: {result['score']:.4f}")
-        print(f"Sentence: {result['sentence_text'][::-1]}")
-    top_k=5
-    return results[:top_k]
+    retrieved_docs= search_engine.run_search(query, top_k=5)
+    # print("\nSemantic Search Results:")
+    # for result in results:
+    #     print(f"Score: {result['score']:.4f}")
+    #     print(f"Sentence: {result['sentence_text'][::-1]}")
+    # top_k=5
+    # return results[:top_k]
     prompt = llm.create_prompt(retrieved_docs, query)
     response = llm.generate_text(prompt)
     return response
